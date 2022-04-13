04d694ade00981feb92131d2bbc9225210832d66
andre <andretnr@gmail.com>
2022-04-13 16:28:29 -0300

    Aplicando validação customizada com Validator

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,10 +1,31 @@
+import { IsDate, IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNomeDeUsuarioUnico } from "./is-nome-de-usuario-unico.validator";
 
 export class Usuario {
 
+    @IsNotEmpty()
+    @IsNumber()
     id: number;
+
+    @IsNomeDeUsuarioUnico({
+        message: 'Este usuário já existe'
+    })
     nomeDeUsuario: string;
+
+    @IsNotEmpty()
+    @IsEmail({}, {
+        message: 'E-mail está incorreto'
+    })
     email: string;
+
+    @IsNotEmpty()
     senha: string;
+
+    @IsNotEmpty({
+        message: 'Nome completo é obrigatório'
+    })
+    @IsString()
     nomeCompleto: string;
+
     dataEntrada: Date;
 }
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { IsNomeDeUsuarioUnicoConstraint } from './is-nome-de-usuario-unico.validator';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
 
 @Module({
-    controllers: [
-        UsuarioController
-      ],
-      providers: [
-        UsuarioService
-      ],
+  controllers: [
+    UsuarioController
+  ],
+  providers: [
+    UsuarioService,
+    IsNomeDeUsuarioUnicoConstraint
+  ],
 })
-export class UsuarioModule {}
+export class UsuarioModule { }
